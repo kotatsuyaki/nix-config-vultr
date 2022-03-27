3e9eb064272fbba5769b055758c30a0756219ded
Akitaki <robinhuang123@gmail.com>
2022-03-27 12:16:02 +0800

    postgres.nix: Disable TCPIP connection

--- modules/postgres.nix
+++ modules/postgres.nix
@@ -2,7 +2,7 @@
   services.postgresql = {
     package = pkgs.postgresql_13;
     enable = true;
-    enableTCPIP = true;
+    enableTCPIP = false;
     settings.unix_socket_permissions = "0770";
   };
 }
